719136a456ebe48246fcf85666d18e95efc5cd72
abudsistem <abudsistem@gmail.com>
2024-09-30 16:11:30 -0500

    FSO-P3-a Aplicacion a internet y CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 
 let notes = [
@@ -27,6 +28,8 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+//const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -87,7 +90,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
