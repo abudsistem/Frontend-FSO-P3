462364bfa74f205a85def028d93c48ceb39b7b2c
abudsistem <abudsistem@gmail.com>
2024-10-02 19:33:40 -0500

    uibuild

--- mongo.js
+++ mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:fullstackpassword@cluster0.dvseu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  `mongodb+srv://fullstack:${password}@cluster0.dvseu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
